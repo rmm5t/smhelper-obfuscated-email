507ad40e05d109bc2a963734483ce453e9e99a80
Ryan McGeary <ryanongit@mcgeary.org>
2008-03-16 12:48:45 -0400

    simplified obfuscation to make it more readable and faster

--- obfuscated_email_helper.rb
+++ obfuscated_email_helper.rb
@@ -24,8 +24,7 @@ module ObfuscatedEmailHelper
   def obfuscated_link(title, href = "", options = {})
     chars, random = [], rand(255)
     plain = link(title, href, options)
-    plain.each_byte { |c| chars << c + random }
-    chars.map! { |c| "#{c}-#{random}"}
+    plain.each_byte { |c| chars << "#{c + random}-#{random}" }
     tag(:script, :type => "text/javascript") do
       "document.write(String.fromCharCode(#{chars.join(',')}));"
     end
